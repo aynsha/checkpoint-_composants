d79b001610c2b0842f421f4e6c19386e3fe4ef36
aynsha <aissatou.fall240@gmail.com>
2024-01-20 23:59:38 +0000

    first commit

--- src/product.js
+++ src/product.js
@@ -1,5 +1,5 @@
 const product={
-  name: 'Sac Coachy',
+  name: 'Sac Ama',
   price: '$350.98',
   description: 'Ceci est une description du produit. Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
   imageUrl: 'img/Coach x Billy Reid.jpg'
